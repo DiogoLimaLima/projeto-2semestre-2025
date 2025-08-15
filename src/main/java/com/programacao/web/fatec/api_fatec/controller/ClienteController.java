3be789a5071e80125fac6315e25e0b18b7fa7a60
DiogoLimaLima <dlgomes944@gmail.com>
2025-08-14 22:48:48 -0300

    feat: add clienteController to API

---
+++ src/main/java/com/programacao/web/fatec/api_fatec/controller/ClienteController.java
@@ -0,0 +1,34 @@
+package com.programacao.web.fatec.api_fatec.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@RestController
+@RequestMapping("/api/clientes")
+public class ClienteController {
+
+    @GetMapping("/testeCliente1") //-> /api/clientes/testeCliente1
+    public String TesteClient(){
+        return "Teste Cliente";
+    }
+    @GetMapping("/testeCliente2/{nome}") //-> /api/clientes/testeCliente2
+    public String Teste(@PathVariable String nome) {
+        return nome;
+    }
+
+    @GetMapping("/testeCliente3/{Idade}")
+    public String Teste_idade(@PathVariable int Idade) {
+        if (Idade >= 18) {
+            return "Maior de idade";
+        } else {
+            return "Menor de idade";
+        }
+    }
+    
+    
+}
