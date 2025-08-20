b3edce906151cc5a6a206399ee75ec40dcfc2c85
DiogoLimaLima <dlgomes944@gmail.com>
2025-08-20 19:40:27 -0300

    feat: post mapping para id e nome

--- src/main/java/com/programacao/web/fatec/api_fatec/controller/ClienteController.java
+++ src/main/java/com/programacao/web/fatec/api_fatec/controller/ClienteController.java
@@ -1,10 +1,16 @@
 package com.programacao.web.fatec.api_fatec.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.programacao.web.fatec.api_fatec.entities.Cliente;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
@@ -29,6 +35,10 @@ public class ClienteController {
             return "Menor de idade";
         }
     }
-    
+
+    @PostMapping("")
+    public String createCliente(@RequestBody Cliente cliente) {
+        return cliente;
+    }
     
 }
