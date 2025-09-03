4b81de04e583ec396d3622f495cadc0cee6d9d1e
DiogoLimaLima <dlgomes944@gmail.com>
2025-09-03 19:41:21 -0300

    feat: banco de cidades e conexão com o banco de clientes

--- src/main/java/com/programacao/web/fatec/api_fatec/entities/Cliente.java
+++ src/main/java/com/programacao/web/fatec/api_fatec/entities/Cliente.java
@@ -1,10 +1,14 @@
 package com.programacao.web.fatec.api_fatec.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -13,11 +17,16 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
     @Column(nullable = false, length = 60)
     private String nome;
     @Column(nullable = false, length = 100)
     private String Endereco;
+    @ManyToOne
+    @JoinColumn(name = "cidade_id")
+    @JsonIgnoreProperties("clientes")
+    // Descomente a linha abaixo para obter o FORMATO 1 (sem o objeto cidade)
+    //@JsonIgnore
+    private Cidade cidade;
 
     public Cliente() {}
 
